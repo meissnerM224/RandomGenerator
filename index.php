7cb6a58117cf0b01c4abe4608ed3a3953430d62a
meissnerM224 <marten_meissner@teilnehmer.cdemy.de>
2023-09-29 22:56:15 +0200

    test roll dice function

--- index.php
+++ index.php
@@ -4,12 +4,14 @@ ini_set('display-errors', 'On');
 function rollDivce(int $size,int $number ){
 
     $max=$size;
-    $ramdom= mt_rand(1,$max);
-     $list=array();
-     for($i =0; $i < $number; $i++){
-         $list= $list($ramdom);
-         echo($ramdom);
-    }
-    return $ramdom;
+    $list=array();
+    for($i = 0; $i < $number; $i++){
+         $ramdom= mt_rand(1,$max);
+        array_push($list,' ', $ramdom);
+         print_r($ramdom);
+        }
+    return $list;
+
 }
+rollDivce(20,5);
 ?>
