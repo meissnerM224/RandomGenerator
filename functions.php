4bf4d50fd05e4f55bf934b601ae8d5e0a65f701c
meissnerM224 <marten_meissner@teilnehmer.cdemy.de>
2023-09-30 00:35:37 +0200

    read input and return value

--- functions.php
+++ functions.php
@@ -3,14 +3,22 @@
 error_reporting(-1);
 ini_set('display-errors', 'On');
 
-print_r($_REQUEST);
 function rollDivce(int $size,int $number ){
-    $max=$size;
-    $list=array();
+    $max = $size;
+    $list = array();
     for($i = 0; $i < $number; $i++){
-         $ramdom= mt_rand(1,$max);
-        array_push($list,' ', $ramdom);
-         print_r($ramdom);
-        }
+        $ramdom = mt_rand(1,$max);
+        array_push($list, $ramdom);
+        print_r($ramdom);
+    }
     return $list;
 }
+$input = array($_REQUEST);
+$number = array_pop($_REQUEST);
+print_r($number);
+print_r(' ');
+$size = array_pop($_REQUEST);
+print_r($size);
+print_r(' ');
+ 
+var_dump( rollDivce( intval($size),intval($number)));
--- index.php
+++ index.php
@@ -3,25 +3,25 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatilbe" content="IE=edge">
     <meta name="viewport" content="width=device-width">
     <title>Dicey Times</title>
 </head>
 
 <body>
-    <h1>Dicey Times!</h1>
-    <h3>Roll the Dice and See Who Wins</h3>
+    <h1>Roll the Dice and See Who Wins</h1>
     <div>
-        <form id="activ_roll_dice" action="functions.php">
+        <form id="activ_dice" action="functions.php">
             <label for="diceSize_ID">How many faces have you dice?</label>
             <br>
-            <input type="text" id="diceSize_ID">
-            <br>
+            <input type="text" id="diceSize_ID" name="diceSize">
+            <br> 
             <label for="dicetryes_ID">How often want to throw?</label>
-            <br>
-            <input type="text" id="dicetryes_ID">
              <br>
-          <!--  <input type="submit"> -->
-            <a href="#" onclick="document.getElementById('activ_roll_dice').submit()">roll the Dice</a>
+            <input type="text" id="dicetryes_ID" name="diceNum"> 
+            <br>
+            <!-- <input type="submit"> -->
+            <a href="#" onclick="document.getElementById('activ_dice').submit()">Roll the Dice</a>
         </form>
     </div>
 </body>
