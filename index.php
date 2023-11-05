2a1e38636d717c2844ea4705f9d8f2c0e35de5b5
meissnerM224 <marten_meissner@teilnehmer.cdemy.de>
2023-11-05 21:09:44 +0100

    add some bootstrap classes

--- index.php
+++ index.php
@@ -1,35 +1,51 @@
 <!DOCTYPE html>
-<html>
-
+<html lang="de">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatilbe" content="IE=edge">
-    <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link type="text/css" href="assets/styles.css">
+    <link rel="stylesheet" href="assets/styles.css">
     <title>Dicey Times</title>
 </head>
-
 <body>
-    <header class="jumbotron">
+    <!-- <header class= "mt-3 p-3 bg-primary text-white rounded"> -->
+    <header class= "alert alert-dark">
         <div class="container">
             <h1>Roll the Dice and See Who Wins</h1>
         </div>
     </header>
-    <div>
-        <form id="activ_dice" action="functions.php">
-            <label for="diceSize_ID">How many faces have you dice?</label>
-            <br>
-            <input type="text" id="diceSize_ID" name="diceSize">
-            <br> 
-            <label for="dicetryes_ID">How often want to throw?</label>
-             <br>
-            <input type="text" id="dicetryes_ID" name="diceNum"> 
-            <br>
-            <!-- <input type="submit"> -->
-            <a href="#" onclick="document.getElementById('activ_dice').submit()">Roll the Dice</a>
-        </form>
-    </div>
+        <section class="container" id="products">
+            <form id="activ_dice" action="functions.php">
+                <div class="row">
+                    <div class ="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <label for="diceSize_ID">How many faces?</label>
+                                <br>
+                                <input type="text" id="diceSize_ID" name="diceSize">
+                            </div>
+                        </div>
+                    </div>
+                    <div class= "col">
+
+                        <div class="card">
+                            <div class ="card-body">
+                                <label for="dicetryes_ID">how often to roll the throw?</label>
+                                <br>
+                                <input type="text"  id="dicetryes_ID" name="diceNum"> 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="justify-content-center">
+
+                    </div>
+                        <div class= "alert alert-secondary">
+                        <a href="#" class="btn btn-primary" onclick="document.getElementById('activ_dice').submit()">Roll the Dice</a>
+                    </div>
+                </div>   
+            </form>
+        </section>
     <script src="assets\js\bootstrap.bundle.js"></script>
 </body>
 
