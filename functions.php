0e95aa09e3cb93b578fe7bb36a4f3306fc3fffc8
meissnerM224 <marten_meissner@teilnehmer.cdemy.de>
2023-11-09 22:01:31 +0100

    exclude some html for better readable

--- functions.php
+++ functions.php
@@ -3,22 +3,24 @@
 error_reporting(-1);
 ini_set('display-errors', 'On');
 
-function rollDice(int $size,int $number ){
+function rollDice(int $size,int $tries ){
     $max = $size;
-    $list = array();
-    for($i = 0; $i < $number; $i++){
-        $ramdom = mt_rand(1,$max);
-        array_push($list, $ramdom);
-        print_r($ramdom);
+    for($i = 0; $i < $tries; $i++){
+        $result[]=mt_rand(1, $max);
     }
-    return $list;
+    return $result;
 }
+if($_REQUEST['REQUEST METHOD']=== 'GET')
 $input = array($_REQUEST);
 $number = array_pop($_REQUEST);
-print_r($number);
-print_r(' ');
 $size = array_pop($_REQUEST);
-print_r($size);
-print_r(' ');
- print_r(rollDice($size, $number));
+print_r(rollDice($size, $number));
+// print_r($number);
+// print_r(' ');
+// print_r($size);
+// print_r(' ');
+// $list = array();
+// $ramdom = mt_rand(1,$max);
+// array_push($list, $ramdom);
+// print_r($ramdom);
 // var_dump( rollDice( intval($size),intval($number)));
--- index.php
+++ index.php
@@ -1,51 +1,18 @@
 <!DOCTYPE html>
 <html lang="de">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatilbe" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/styles.css">
-    <title>Dicey Times</title>
-</head>
-<body class="bg-success.bg p-2" style="--bs-bg-opacity: .5;">
-    <!-- <header class= "mt-3 p-3 bg-primary text-white rounded"> -->
-    <header class= "alert alert-dark">
-        <div class="container">
-            <h1>Roll the Dice and See Who Wins</h1>
-        </div>
-    </header>
-        <section class="container" id="products">
-            <form id="activ_dice" action="functions.php">
-                <div class="row">
-                    <div class ="col">
-                        <div class="card">
-                            <div class="card-body">
-                                <label for="diceSize_ID">How many faces?</label>
-                                <br>
-                                <input type="text" class="form-control" id="diceSize_ID" name="diceSize">
-                            </div>
-                        </div>
-                    </div>
-                    <div class= "col">
-                        <div class="card">
-                            <div class ="card-body">
-                                <label for="dicetryes_ID">how often to roll the throw?</label>
-                                <br>
-                                <input type="text" class="form-control" id="dicetryes_ID" name="diceNum"> 
-                            </div>
-                        </div>
-                    </div>
-                </div>   
-                    <div class="justify-content-center">   
-                        <div class= "alert alert-secondary container" >
-                            <a href="#" class="btn btn-primary" onclick="document.getElementById('activ_dice').submit()">Roll the Dice</a>
-                        </div>
-                    </div>
-            </form>
-        </section>
-    <script src="assets\js\bootstrap.bundle.js"></script>
-</body>
-
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatilbe" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/styles.css">
+        <title>Dicey Times</title>
+    </head>
+    <body class="bg-success.bg p-2" style="--bs-bg-opacity: .5;">
+        <?php include 'header.php'?>
+        <?php include 'input_section.php'?> 
+        
+        <script src="assets\js\bootstrap.bundle.js"></script>
+    </body>
 </html>
+    <!-- <a href="#" class="btn btn-primary" onclick="document.getElementById('activ_dice').submit()">Roll the Dice</a> --> 
